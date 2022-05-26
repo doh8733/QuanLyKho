f72cfdbb2a6f4e88ddfde80ee0bc6915c95bb0b5
Cuongct23 <cuongquanbai2@gmail.com>
2022-05-26 11:59:05 +0700

    Giao diện splash, hoá đơn

---
+++ app/src/main/java/com/quannm18/quanlykho/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.quannm18.quanlykho;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    int counter=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        prog();
+    }
+
+    private void prog() {
+        progressBar=findViewById(R.id.progressBar);
+        final Timer t=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                progressBar.setProgress(counter);
+                if(counter==100){
+                    t.cancel();
+                }
+            }
+        };
+        t.schedule(task,0,100);
+    }
+}
