aedf2da2454670f3337a088dfc97df217759c87c
NguyenVanDuc <ducnvph14154@fpt.edu.vn>
2022-06-03 18:02:24 +0700

    Giao diện đăng nhập, đổi mật khẩu, giao diện nhân viên

---
+++ app/src/main/java/com/quannm18/quanlykho/EmployeeList.java
@@ -0,0 +1,14 @@
+package com.quannm18.quanlykho;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmployeeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_list);
+    }
+}
